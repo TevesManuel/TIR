b6f64723c8759381c16b7a6c091d43880ec11e8b
Manuel Teves <manuel.tomas.teves@gmail.com>
2024-06-10 16:50:20 -0300

    Switch device

--- firmware/src/app.cpp
+++ firmware/src/app.cpp
@@ -4,15 +4,42 @@
 #define IR_SENSOR 8
 #define LED 13
 
-IRrecv irRecv;
+IRrecv irRecv(IR_SENSOR);
 
-void setup() {
+struct LEDOUT
+{
+    int pulseWidth;
+    int periodWidth;
+    int targetTimes;
+    int actualTimes;
+    unsigned long lastModification;
+} LEDOUT;
+
+void LEDOUT_RESET()
+{
+    LEDOUT.pulseWidth = 10;
+    LEDOUT.periodWidth = 10;
+    LEDOUT.targetTimes = 3;
+    LEDOUT.actualTimes = 0;
+    LEDOUT.lastModification = 0;
+}
+
+void setup()
+{
+    Serial.begin(9600);
+
+    irRecv.enableIRIn();
     pinMode(LED, OUTPUT);
 }
 
-void loop() {
-    digitalWrite(LED, HIGH);
-    delay(1000);
-    digitalWrite(LED, LOW);
-    delay(1000);
+void loop()
+{
+    if (irRecv.decode()) {
+        Serial.println(irRecv.decodedIRData.decodedRawData, HEX);
+        irRecv.resume();
+    }
+    if (LEDOUT.actualTimes < LEDOUT.targetTimes) // Is active?
+    {
+        if
+    }
 }
